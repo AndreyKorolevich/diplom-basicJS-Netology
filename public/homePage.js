e6d897f3eb60a8b17e3060d527a634d1db38d9b5
Andrei Korolevich <korolevich1994@mail.ru>
2020-04-16 19:35:38 +0300

    Operation with money

--- public/homePage.js
+++ public/homePage.js
@@ -39,3 +39,43 @@ const updateRates = () => ApiConnector.getStocks(response => {
 });
 updateRates();
 setInterval(updateRates,60000);
+
+const money = new MoneyManager();
+money.addMoneyCallback = data => ApiConnector.addMoney(data, response => {
+    try {
+        if(response.success === false) {
+           throw new Error('It`s impossible to add mony in your wallet');
+        }
+        money.setMessage(false,'Money has added to your wallet successfully');
+        ProfileWidget.showProfile(response.data);  
+       } catch (err) {
+           money.setMessage(true,err);
+       }
+    return;
+});
+
+money.conversionMoneyCallback = data => ApiConnector.convertMoney(data,response => {
+    try {
+        if(response.success === false) {
+           throw new Error('It`s impossible to convert this');
+        }
+        money.setMessage(false,'The conversion was successful');
+        ProfileWidget.showProfile(response.data);  
+       } catch (err) {
+           money.setMessage(true,err);
+       }
+    return;
+});
+
+money.sendMoneyCallback = data => ApiConnector.transferMoney(data,response => {
+    try {
+        if(response.success === false) {
+           throw new Error('It`s impossible to transfer money');
+        }
+        money.setMessage(false,'The transfer was successful');
+        ProfileWidget.showProfile(response.data);  
+       } catch (err) {
+           money.setMessage(true,err);
+       }
+    return;
+});
